cf248ae5b403108ac70678c65165eafd57992d5b
Ovee Hasnat <abu.hasnat@northsouth.edu>
2024-04-20 15:26:32 +0600

    navbar name added

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,12 +11,15 @@ export default function Navbar() {
       <div className="max-w-[1400px] mx-auto p-4 flex items-center justify-between">
         <img src={logo} alt="Logo" className="w-20" />
 
-        <button
-          className="px-4 py-2 bg-white/20 rounded-lg font-medium hover:bg-white/35"
-          onClick={signOut}
-        >
-          Sign Out
-        </button>
+        <div className="flex items-center gap-2">
+          <p>Hello, {JSON.parse(localStorage.authUser).name}</p>
+          <button
+            className="px-4 py-2 bg-white/20 rounded-lg font-medium hover:bg-white/35"
+            onClick={signOut}
+          >
+            Sign Out
+          </button>
+        </div>
       </div>
     </div>
   );
